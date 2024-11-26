a3dda674165deeb8c9b81c61b40acf69a6324400
MD. Nafiul Azam <137706705+Nafiul-Azam@users.noreply.github.com>
2024-11-26 18:10:04 +0600

    Add files via upload

---
+++ new.js
@@ -0,0 +1,9 @@
+{
+  let name = "Nafiul";
+  const age = 25;
+  console.log(name); // Output: "Nafiul"
+  console.log(age); // Output: 25
+}
+
+// console.log(name); // Error: name is not defined
+// console.log(age);  // Error: age is not defined
